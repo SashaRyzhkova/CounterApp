d91d50842c4a8f99abdf8c37ef667b62f02fd2eb
Sasha Ryzhkova <aoryzhkova@gmail.com>
2023-01-31 00:52:00 +0300

    добавлен код для работы счетчика

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var counterResult: UILabel!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    var counter = 0
+    
+    func updateCounterResult() {
+        counterResult.text = "Значение счётчика: \(counter)"
+    }
+    
+    @IBAction func buttonTouchedUpInside(_ sender: Any) {
+        counter += 1
+        updateCounterResult()
+    }
 }
 
